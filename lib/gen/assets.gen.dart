a372008e29c6a08bff275c7a887ec9431037bb23
Nghinguyenhuu <107188801+Nghinguyenhuu@users.noreply.github.com>
2023-11-20 13:11:55 +0700

    [Fix] update UI

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,542 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter/services.dart';
+
+class $AssetsFontsGen {
+  const $AssetsFontsGen();
+
+  /// File path: assets/fonts/Poppins-Bold.ttf
+  String get poppinsBold => 'assets/fonts/Poppins-Bold.ttf';
+
+  /// File path: assets/fonts/Poppins-Medium.ttf
+  String get poppinsMedium => 'assets/fonts/Poppins-Medium.ttf';
+
+  /// File path: assets/fonts/Poppins-Regular.ttf
+  String get poppinsRegular => 'assets/fonts/Poppins-Regular.ttf';
+
+  /// File path: assets/fonts/Poppins-SemiBold.ttf
+  String get poppinsSemiBold => 'assets/fonts/Poppins-SemiBold.ttf';
+
+  /// List of all assets
+  List<String> get values =>
+      [poppinsBold, poppinsMedium, poppinsRegular, poppinsSemiBold];
+}
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  $AssetsImagesPngGen get png => const $AssetsImagesPngGen();
+  $AssetsImagesSvgGen get svg => const $AssetsImagesSvgGen();
+}
+
+class $AssetsImagesPngGen {
+  const $AssetsImagesPngGen();
+
+  /// File path: assets/images/png/ic_google.png
+  AssetGenImage get icGoogle =>
+      const AssetGenImage('assets/images/png/ic_google.png');
+
+  /// File path: assets/images/png/img_back_facing_after.png
+  AssetGenImage get imgBackFacingAfter =>
+      const AssetGenImage('assets/images/png/img_back_facing_after.png');
+
+  /// File path: assets/images/png/img_back_facing_before.png
+  AssetGenImage get imgBackFacingBefore =>
+      const AssetGenImage('assets/images/png/img_back_facing_before.png');
+
+  /// File path: assets/images/png/img_banner_background.png
+  AssetGenImage get imgBannerBackground =>
+      const AssetGenImage('assets/images/png/img_banner_background.png');
+
+  /// File path: assets/images/png/img_barbell.png
+  AssetGenImage get imgBarbell =>
+      const AssetGenImage('assets/images/png/img_barbell.png');
+
+  /// File path: assets/images/png/img_front_facing.png
+  AssetGenImage get imgFrontFacing =>
+      const AssetGenImage('assets/images/png/img_front_facing.png');
+
+  /// File path: assets/images/png/img_front_facing_after.png
+  AssetGenImage get imgFrontFacingAfter =>
+      const AssetGenImage('assets/images/png/img_front_facing_after.png');
+
+  /// File path: assets/images/png/img_goal_1.png
+  AssetGenImage get imgGoal1 =>
+      const AssetGenImage('assets/images/png/img_goal_1.png');
+
+  /// File path: assets/images/png/img_goal_2.png
+  AssetGenImage get imgGoal2 =>
+      const AssetGenImage('assets/images/png/img_goal_2.png');
+
+  /// File path: assets/images/png/img_goal_3.png
+  AssetGenImage get imgGoal3 =>
+      const AssetGenImage('assets/images/png/img_goal_3.png');
+
+  /// File path: assets/images/png/img_left_facing_after.png
+  AssetGenImage get imgLeftFacingAfter =>
+      const AssetGenImage('assets/images/png/img_left_facing_after.png');
+
+  /// File path: assets/images/png/img_left_facing_before.png
+  AssetGenImage get imgLeftFacingBefore =>
+      const AssetGenImage('assets/images/png/img_left_facing_before.png');
+
+  /// File path: assets/images/png/img_logo.png
+  AssetGenImage get imgLogo =>
+      const AssetGenImage('assets/images/png/img_logo.png');
+
+  /// File path: assets/images/png/img_logo_2.png
+  AssetGenImage get imgLogo2 =>
+      const AssetGenImage('assets/images/png/img_logo_2.png');
+
+  /// File path: assets/images/png/img_lunch_1.png
+  AssetGenImage get imgLunch1 =>
+      const AssetGenImage('assets/images/png/img_lunch_1.png');
+
+  /// File path: assets/images/png/img_lunch_2.png
+  AssetGenImage get imgLunch2 =>
+      const AssetGenImage('assets/images/png/img_lunch_2.png');
+
+  /// File path: assets/images/png/img_onboarding_1.png
+  AssetGenImage get imgOnboarding1 =>
+      const AssetGenImage('assets/images/png/img_onboarding_1.png');
+
+  /// File path: assets/images/png/img_onboarding_2.png
+  AssetGenImage get imgOnboarding2 =>
+      const AssetGenImage('assets/images/png/img_onboarding_2.png');
+
+  /// File path: assets/images/png/img_onboarding_3.png
+  AssetGenImage get imgOnboarding3 =>
+      const AssetGenImage('assets/images/png/img_onboarding_3.png');
+
+  /// File path: assets/images/png/img_onboarding_4.png
+  AssetGenImage get imgOnboarding4 =>
+      const AssetGenImage('assets/images/png/img_onboarding_4.png');
+
+  /// File path: assets/images/png/img_progress_photo_2.png
+  AssetGenImage get imgProgressPhoto2 =>
+      const AssetGenImage('assets/images/png/img_progress_photo_2.png');
+
+  /// File path: assets/images/png/img_register_profile.png
+  AssetGenImage get imgRegisterProfile =>
+      const AssetGenImage('assets/images/png/img_register_profile.png');
+
+  /// File path: assets/images/png/img_register_success.png
+  AssetGenImage get imgRegisterSuccess =>
+      const AssetGenImage('assets/images/png/img_register_success.png');
+
+  /// File path: assets/images/png/img_right_facing_after.png
+  AssetGenImage get imgRightFacingAfter =>
+      const AssetGenImage('assets/images/png/img_right_facing_after.png');
+
+  /// File path: assets/images/png/img_right_facing_before.png
+  AssetGenImage get imgRightFacingBefore =>
+      const AssetGenImage('assets/images/png/img_right_facing_before.png');
+
+  /// File path: assets/images/png/img_skipping_rope.png
+  AssetGenImage get imgSkippingRope =>
+      const AssetGenImage('assets/images/png/img_skipping_rope.png');
+
+  /// File path: assets/images/png/img_sleep_graph.png
+  AssetGenImage get imgSleepGraph =>
+      const AssetGenImage('assets/images/png/img_sleep_graph.png');
+
+  /// File path: assets/images/png/img_target_today.png
+  AssetGenImage get imgTargetToday =>
+      const AssetGenImage('assets/images/png/img_target_today.png');
+
+  /// File path: assets/images/png/img_target_today_1.png
+  AssetGenImage get imgTargetToday1 =>
+      const AssetGenImage('assets/images/png/img_target_today_1.png');
+
+  /// File path: assets/images/png/img_workout_1.png
+  AssetGenImage get imgWorkout1 =>
+      const AssetGenImage('assets/images/png/img_workout_1.png');
+
+  /// File path: assets/images/png/img_workout_2.png
+  AssetGenImage get imgWorkout2 =>
+      const AssetGenImage('assets/images/png/img_workout_2.png');
+
+  /// File path: assets/images/png/img_workout_3.png
+  AssetGenImage get imgWorkout3 =>
+      const AssetGenImage('assets/images/png/img_workout_3.png');
+
+  /// File path: assets/images/png/img_workout_details_video.png
+  AssetGenImage get imgWorkoutDetailsVideo =>
+      const AssetGenImage('assets/images/png/img_workout_details_video.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+        icGoogle,
+        imgBackFacingAfter,
+        imgBackFacingBefore,
+        imgBannerBackground,
+        imgBarbell,
+        imgFrontFacing,
+        imgFrontFacingAfter,
+        imgGoal1,
+        imgGoal2,
+        imgGoal3,
+        imgLeftFacingAfter,
+        imgLeftFacingBefore,
+        imgLogo,
+        imgLogo2,
+        imgLunch1,
+        imgLunch2,
+        imgOnboarding1,
+        imgOnboarding2,
+        imgOnboarding3,
+        imgOnboarding4,
+        imgProgressPhoto2,
+        imgRegisterProfile,
+        imgRegisterSuccess,
+        imgRightFacingAfter,
+        imgRightFacingBefore,
+        imgSkippingRope,
+        imgSleepGraph,
+        imgTargetToday,
+        imgTargetToday1,
+        imgWorkout1,
+        imgWorkout2,
+        imgWorkout3,
+        imgWorkoutDetailsVideo
+      ];
+}
+
+class $AssetsImagesSvgGen {
+  const $AssetsImagesSvgGen();
+
+  /// File path: assets/images/svg/ic_2_user.svg
+  SvgGenImage get ic2User =>
+      const SvgGenImage('assets/images/svg/ic_2_user.svg');
+
+  /// File path: assets/images/svg/ic_achievement.svg
+  SvgGenImage get icAchievement =>
+      const SvgGenImage('assets/images/svg/ic_achievement.svg');
+
+  /// File path: assets/images/svg/ic_activity.svg
+  SvgGenImage get icActivity =>
+      const SvgGenImage('assets/images/svg/ic_activity.svg');
+
+  /// File path: assets/images/svg/ic_activity_active.svg
+  SvgGenImage get icActivityActive =>
+      const SvgGenImage('assets/images/svg/ic_activity_active.svg');
+
+  /// File path: assets/images/svg/ic_arrow_right_circle.svg
+  SvgGenImage get icArrowRightCircle =>
+      const SvgGenImage('assets/images/svg/ic_arrow_right_circle.svg');
+
+  /// File path: assets/images/svg/ic_back.svg
+  SvgGenImage get icBack => const SvgGenImage('assets/images/svg/ic_back.svg');
+
+  /// File path: assets/images/svg/ic_calendar.svg
+  SvgGenImage get icCalendar =>
+      const SvgGenImage('assets/images/svg/ic_calendar.svg');
+
+  /// File path: assets/images/svg/ic_camera.svg
+  SvgGenImage get icCamera =>
+      const SvgGenImage('assets/images/svg/ic_camera.svg');
+
+  /// File path: assets/images/svg/ic_camera_active.svg
+  SvgGenImage get icCameraActive =>
+      const SvgGenImage('assets/images/svg/ic_camera_active.svg');
+
+  /// File path: assets/images/svg/ic_cancel.svg
+  SvgGenImage get icCancel =>
+      const SvgGenImage('assets/images/svg/ic_cancel.svg');
+
+  /// File path: assets/images/svg/ic_card_details.svg
+  SvgGenImage get icCardDetails =>
+      const SvgGenImage('assets/images/svg/ic_card_details.svg');
+
+  /// File path: assets/images/svg/ic_cheveron_rignt.svg
+  SvgGenImage get icCheveronRignt =>
+      const SvgGenImage('assets/images/svg/ic_cheveron_rignt.svg');
+
+  /// File path: assets/images/svg/ic_details.svg
+  SvgGenImage get icDetails =>
+      const SvgGenImage('assets/images/svg/ic_details.svg');
+
+  /// File path: assets/images/svg/ic_dropdown.svg
+  SvgGenImage get icDropdown =>
+      const SvgGenImage('assets/images/svg/ic_dropdown.svg');
+
+  /// File path: assets/images/svg/ic_facebook.svg
+  SvgGenImage get icFacebook =>
+      const SvgGenImage('assets/images/svg/ic_facebook.svg');
+
+  /// File path: assets/images/svg/ic_follow.svg
+  SvgGenImage get icFollow =>
+      const SvgGenImage('assets/images/svg/ic_follow.svg');
+
+  /// File path: assets/images/svg/ic_google.svg
+  SvgGenImage get icGoogle =>
+      const SvgGenImage('assets/images/svg/ic_google.svg');
+
+  /// File path: assets/images/svg/ic_hide.svg
+  SvgGenImage get icHide => const SvgGenImage('assets/images/svg/ic_hide.svg');
+
+  /// File path: assets/images/svg/ic_home.svg
+  SvgGenImage get icHome => const SvgGenImage('assets/images/svg/ic_home.svg');
+
+  /// File path: assets/images/svg/ic_home_active.svg
+  SvgGenImage get icHomeActive =>
+      const SvgGenImage('assets/images/svg/ic_home_active.svg');
+
+  /// File path: assets/images/svg/ic_home_activity.svg
+  SvgGenImage get icHomeActivity =>
+      const SvgGenImage('assets/images/svg/ic_home_activity.svg');
+
+  /// File path: assets/images/svg/ic_lock.svg
+  SvgGenImage get icLock => const SvgGenImage('assets/images/svg/ic_lock.svg');
+
+  /// File path: assets/images/svg/ic_login.svg
+  SvgGenImage get icLogin =>
+      const SvgGenImage('assets/images/svg/ic_login.svg');
+
+  /// File path: assets/images/svg/ic_message.svg
+  SvgGenImage get icMessage =>
+      const SvgGenImage('assets/images/svg/ic_message.svg');
+
+  /// File path: assets/images/svg/ic_message_gradient.svg
+  SvgGenImage get icMessageGradient =>
+      const SvgGenImage('assets/images/svg/ic_message_gradient.svg');
+
+  /// File path: assets/images/svg/ic_notification.svg
+  SvgGenImage get icNotification =>
+      const SvgGenImage('assets/images/svg/ic_notification.svg');
+
+  /// File path: assets/images/svg/ic_notify.svg
+  SvgGenImage get icNotify =>
+      const SvgGenImage('assets/images/svg/ic_notify.svg');
+
+  /// File path: assets/images/svg/ic_plus.svg
+  SvgGenImage get icPlus => const SvgGenImage('assets/images/svg/ic_plus.svg');
+
+  /// File path: assets/images/svg/ic_policy.svg
+  SvgGenImage get icPolicy =>
+      const SvgGenImage('assets/images/svg/ic_policy.svg');
+
+  /// File path: assets/images/svg/ic_policy_gradient.svg
+  SvgGenImage get icPolicyGradient =>
+      const SvgGenImage('assets/images/svg/ic_policy_gradient.svg');
+
+  /// File path: assets/images/svg/ic_profile.svg
+  SvgGenImage get icProfile =>
+      const SvgGenImage('assets/images/svg/ic_profile.svg');
+
+  /// File path: assets/images/svg/ic_profile_active.svg
+  SvgGenImage get icProfileActive =>
+      const SvgGenImage('assets/images/svg/ic_profile_active.svg');
+
+  /// File path: assets/images/svg/ic_profile_gradient.svg
+  SvgGenImage get icProfileGradient =>
+      const SvgGenImage('assets/images/svg/ic_profile_gradient.svg');
+
+  /// File path: assets/images/svg/ic_progress_photo.svg
+  SvgGenImage get icProgressPhoto =>
+      const SvgGenImage('assets/images/svg/ic_progress_photo.svg');
+
+  /// File path: assets/images/svg/ic_setting.svg
+  SvgGenImage get icSetting =>
+      const SvgGenImage('assets/images/svg/ic_setting.svg');
+
+  /// File path: assets/images/svg/ic_show.svg
+  SvgGenImage get icShow => const SvgGenImage('assets/images/svg/ic_show.svg');
+
+  /// File path: assets/images/svg/ic_swap.svg
+  SvgGenImage get icSwap => const SvgGenImage('assets/images/svg/ic_swap.svg');
+
+  /// File path: assets/images/svg/ic_weight.svg
+  SvgGenImage get icWeight =>
+      const SvgGenImage('assets/images/svg/ic_weight.svg');
+
+  /// File path: assets/images/svg/ic_workout.svg
+  SvgGenImage get icWorkout =>
+      const SvgGenImage('assets/images/svg/ic_workout.svg');
+
+  /// List of all assets
+  List<SvgGenImage> get values => [
+        ic2User,
+        icAchievement,
+        icActivity,
+        icActivityActive,
+        icArrowRightCircle,
+        icBack,
+        icCalendar,
+        icCamera,
+        icCameraActive,
+        icCancel,
+        icCardDetails,
+        icCheveronRignt,
+        icDetails,
+        icDropdown,
+        icFacebook,
+        icFollow,
+        icGoogle,
+        icHide,
+        icHome,
+        icHomeActive,
+        icHomeActivity,
+        icLock,
+        icLogin,
+        icMessage,
+        icMessageGradient,
+        icNotification,
+        icNotify,
+        icPlus,
+        icPolicy,
+        icPolicyGradient,
+        icProfile,
+        icProfileActive,
+        icProfileGradient,
+        icProgressPhoto,
+        icSetting,
+        icShow,
+        icSwap,
+        icWeight,
+        icWorkout
+      ];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsFontsGen fonts = $AssetsFontsGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(this._assetName);
+
+  final String _assetName;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = false,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.low,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    String? package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
+
+class SvgGenImage {
+  const SvgGenImage(this._assetName);
+
+  final String _assetName;
+
+  SvgPicture svg({
+    Key? key,
+    bool matchTextDirection = false,
+    AssetBundle? bundle,
+    String? package,
+    double? width,
+    double? height,
+    BoxFit fit = BoxFit.contain,
+    AlignmentGeometry alignment = Alignment.center,
+    bool allowDrawingOutsideViewBox = false,
+    WidgetBuilder? placeholderBuilder,
+    String? semanticsLabel,
+    bool excludeFromSemantics = false,
+    SvgTheme theme = const SvgTheme(),
+    ColorFilter? colorFilter,
+    Clip clipBehavior = Clip.hardEdge,
+    @deprecated Color? color,
+    @deprecated BlendMode colorBlendMode = BlendMode.srcIn,
+    @deprecated bool cacheColorFilter = false,
+  }) {
+    return SvgPicture.asset(
+      _assetName,
+      key: key,
+      matchTextDirection: matchTextDirection,
+      bundle: bundle,
+      package: package,
+      width: width,
+      height: height,
+      fit: fit,
+      alignment: alignment,
+      allowDrawingOutsideViewBox: allowDrawingOutsideViewBox,
+      placeholderBuilder: placeholderBuilder,
+      semanticsLabel: semanticsLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      theme: theme,
+      colorFilter: colorFilter,
+      color: color,
+      colorBlendMode: colorBlendMode,
+      clipBehavior: clipBehavior,
+      cacheColorFilter: cacheColorFilter,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
